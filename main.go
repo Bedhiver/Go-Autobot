5658f8e2bc34c52999f2bc7479e7ed49530d6e6b
Francisco Fernandez <francisco59553@gmail.com>
2022-03-12 22:18:41 +0100

    :poop: experiments func

--- main.go
+++ main.go
@@ -3,38 +3,39 @@ package main
 import (
 	"example/greetings"
 	"fmt"
-	"log"
 	"github.com/go-vgo/robotgo"
+	// hook "github.com/robotn/gohook"
+	"log"
 	// "rsc.io/quote"
 )
 
 func main() {
 	robotgo.MouseSleep = 100
+	// hook.Register(hook.KeyDown, []string{"e"}, func(e hook.Event) {
+	// 	fmt.Println("key pressed was e")
+	// 	hook.End()
+	// })
 
-	// robotgo.ScrollMouse(10, "up")
-	// robotgo.ScrollMouse(20, "right")
-  
-	// robotgo.Scroll(0, -10)
-	// robotgo.Scroll(100, 0)
-  
-	// robotgo.MilliSleep(100)
-	// robotgo.ScrollSmooth(-10, 6)
-  
-	// robotgo.Move(10, 20)
-	// robotgo.MoveRelative(0, -10)
-	// robotgo.DragSmooth(10, 10)
-  
-	// robotgo.Click("wheelRight")
-	// robotgo.Click("left", true)
-	robotgo.MoveSmooth(100, 200, 1.0, 10.0)
-  
-	// robotgo.Toggle("left")
-	// robotgo.Toggle("left", "up")
+	// s := hook.Start()
+	// <-hook.Process(s)
+
+	for {
+		// eventPressed := robotgo.AddEvent("e")
+		// if eventPressed {
+		// 	break
+		// }
+		robotgo.MoveSmooth(500, 500, 1.0, 1.0)
+		robotgo.MoveSmooth(1000, 500, 1.0, 1.0)
+		// fmt.Println(robotgo.GetMousePos())
+		// if condition {
+
+		// }
+	}
 
 	log.SetPrefix("greetings : ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("d")
 	if err != nil {
 		log.Fatal(err)
 	}
