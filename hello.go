d6b816c01baa3d8e7d5d2f6e87808d025c19591e
Francisco Fernandez <francisco59553@gmail.com>
2022-03-12 18:36:59 +0100

    :goal_net: error catching

--- hello.go
+++ hello.go
@@ -3,12 +3,19 @@ package main
 import (
 	"example/greetings"
 	"fmt"
-	"rsc.io/quote"
+	"log"
+	// "rsc.io/quote"
 )
 
 func main() {
-	message := greetings.Hello("paqui")
+	log.SetPrefix("greetings : ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("ok")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
-	fmt.Println("hello world !")
-	fmt.Println(quote.Go())
+	// fmt.Println("hello world !")
+	// fmt.Println(quote.Go())
 }
