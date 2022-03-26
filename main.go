19dd4a4bcd521125270b8354657eea185b7aa91f
Francisco Fernandez <francisco59553@gmail.com>
2022-03-26 21:04:15 +0100

    :sparkles: listen for loop

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"example/greetings"
+	// "example/greetings"
 	"fmt"
 	"github.com/go-vgo/robotgo"
-	// hook "github.com/robotn/gohook"
-	"log"
+	hook "github.com/robotn/gohook"
+	// "log"
 	// "rsc.io/quote"
 )
 
 func main() {
-	robotgo.MouseSleep = 100
+
+	add()
+
+	// robotgo.MouseSleep = 100
 	// hook.Register(hook.KeyDown, []string{"e"}, func(e hook.Event) {
 	// 	fmt.Println("key pressed was e")
 	// 	hook.End()
@@ -19,27 +22,65 @@ func main() {
 	// s := hook.Start()
 	// <-hook.Process(s)
 
-	for {
-		// eventPressed := robotgo.AddEvent("e")
-		// if eventPressed {
-		// 	break
-		// }
-		robotgo.MoveSmooth(500, 500, 1.0, 1.0)
-		robotgo.MoveSmooth(1000, 500, 1.0, 1.0)
-		// fmt.Println(robotgo.GetMousePos())
-		// if condition {
+	// condition := false
 
-		// }
-	}
+	// for {
+	// 	// eventPressed := robotgo.AddEvent("e")
+	// 	// if eventPressed {
+	// 	// 	break
+	// 	// }
+	// 	robotgo.MoveSmooth(500, 500, 1.0, 1.0)
+	// 	robotgo.MoveSmooth(1000, 500, 1.0, 1.0)
+	// 	// fmt.Println(robotgo.GetMousePos())
+	// 	if condition {
+	// 		break
+	// 	}
+	// }
 
-	log.SetPrefix("greetings : ")
-	log.SetFlags(0)
+	fmt.Println("Press any key to exit")
+	// var input string
+	// fmt.Scanf(input, "%s")
 
-	message, err := greetings.Hello("d")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(message)
+	// log.SetPrefix("greetings : ")
+	// log.SetFlags(0)
+
+	// message, err := greetings.Hello("d")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(message)
 	// fmt.Println("hello world !")
 	// fmt.Println(quote.Go())
+}
+
+func add() {
+
+	// condition := false
+
+	s := hook.Start()
+	defer hook.End()
+
+	// ct := false
+
+	for {
+
+		i := <-s
+
+		// fmt.Println(i)
+		defer robotgo.MoveSmooth(500, 500, 1.0, 1.0)
+		robotgo.MoveSmooth(1000, 500, 1.0, 1.0)
+
+		// if i.Kind == hook.KeyHold && i.Rawcode == 59 {
+		if i.Rawcode == 101 {
+			fmt.Println("HELLLOOOOOO")
+			// ct = true
+
+			break
+		}
+		// if ct && i.Rawcode == 12 {
+		// if ct {
+		// 	break
+		// }
+	}
+
 }
